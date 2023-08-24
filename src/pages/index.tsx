e652c01c53c37089e5f302264b75df9d0f797a96
Lukas Polak <lukash.polak@gmail.com>
2023-08-24 11:48:03 +0200

    feat: implement getServerSideProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,12 @@
+import { GetServerSideProps } from "next"
+import {
+  getClasses,
+  getFacilities,
+  getInstructors,
+  getLocations,
+  getSports,
+} from "@/api"
+
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center w-screen h-screen bg-gray-900 text-white">
@@ -6,4 +15,22 @@ export default function Home() {
       </h1>
     </main>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async () => {
+  const classes = await getClasses()
+  const facilities = await getFacilities()
+  const instructors = await getInstructors()
+  const locations = await getLocations()
+  const sports = await getSports()
+
+  return {
+    props: {
+      classes,
+      facilities,
+      instructors,
+      locations,
+      sports,
+    },
+  }
 }
