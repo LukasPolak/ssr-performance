fcc167d6a7b6ad0edf34e09d9161fe294b73ee69
Lukas Polak <lukash.polak@gmail.com>
2023-08-24 11:47:51 +0200

    feat: initialize api fetcher functions

---
+++ src/api/index.ts
@@ -0,0 +1,29 @@
+export const getLocations = async () => {
+  return await fetch("http://localhost:3000/api/hello").then((res) =>
+    res.json()
+  )
+}
+
+export const getFacilities = async () => {
+  return await fetch("http://localhost:3000/api/hello").then((res) =>
+    res.json()
+  )
+}
+
+export const getSports = async () => {
+  return await fetch("http://localhost:3000/api/hello").then((res) =>
+    res.json()
+  )
+}
+
+export const getInstructors = async () => {
+  return await fetch("http://localhost:3000/api/hello").then((res) =>
+    res.json()
+  )
+}
+
+export const getClasses = async () => {
+  return await fetch("http://localhost:3000/api/hello").then((res) =>
+    res.json()
+  )
+}
