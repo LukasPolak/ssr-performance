0482c35091e44453ff2ab735026a32bdd811dfce
Lukas Polak <lukash.polak@gmail.com>
2023-08-24 11:47:42 +0200

    fix: update api endpoint

--- src/pages/api/hello.ts
+++ src/pages/api/hello.ts
@@ -1,13 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-type Data = {
-  name: string
-}
-
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  res.status(200).json({ name: 'John Doe' })
+export default function handler(_req: NextApiRequest, res: NextApiResponse) {
+  setTimeout(() => {
+    res.status(200).json({})
+  }, Math.random() * 500 + 500)
 }
